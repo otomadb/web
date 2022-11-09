3917cd5364241db87111db60c7b4492a7d16bda6
SnO2WMaN <me@sno2wman.net>
2022-11-09 22:41:24 +0900

    fix tailwindcss setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {},
