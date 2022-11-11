37248e960a325b356b27a795e3a77dbfaa3db9ab
SnO2WMaN <me@sno2wman.net>
2022-11-12 00:16:41 +0900

    tag page

--- src/app/tags/[id]/getData.ts
+++ src/app/tags/[id]/getData.ts
@@ -1,11 +1,22 @@
+export const apiUrl = (id: string) => `http://localhost:8080/tags/${id}`;
+
 export const getData = async (
   id: string
 ): Promise<{
   id: string;
   name_primary: string;
+  context: {
+    id: string;
+    name_primary: string;
+  } | null;
+  tagged_videos: {
+    id: string;
+    title_primary: string;
+    image_primary: string;
+  }[];
 }> => {
-  return {
-    id: "2",
-    name_primary: "ぼっち・ざ・ろっく！",
-  };
+  const res = await fetch(apiUrl(id), { cache: "default" });
+  if (!res.ok) throw new Error(`${res.status}`);
+
+  return res.json();
 };
--- src/app/tags/[id]/page.tsx
+++ src/app/tags/[id]/page.tsx
@@ -1,11 +1,34 @@
+import clsx from "clsx";
+
+import { VideoList } from "~/app/videos/[id]/VideoList";
+
 import { getData } from "./getData";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const details = await getData(params.id);
 
   return (
-    <main>
-      <h1>{details.name_primary}</h1>
+    <main className={clsx(["container"], ["mx-auto"])}>
+      <h1 className={clsx(["flex"], ["items-center"])}>
+        <span className={clsx(["block"], ["text-2xl"], ["text-slate-800"])}>
+          {details.name_primary}
+        </span>
+        {details.context && (
+          <span
+            className={clsx(
+              ["block"],
+              ["ml-1"],
+              ["text-md"],
+              ["text-slate-500"]
+            )}
+          >
+            ({details.context.name_primary})
+          </span>
+        )}
+      </h1>
+      <div className={clsx(["mt-4"])}>
+        <VideoList className={clsx()} videos={details.tagged_videos} />
+      </div>
     </main>
   );
 };
