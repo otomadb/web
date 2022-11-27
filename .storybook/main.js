0dc6ed7adcef8366a290b1567c241eeb421e9aa8
SnO2WMaN <me@sno2wman.net>
2022-11-27 13:03:47 +0900

    update storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,20 @@
+const path = require("path");
+const tsconfigPaths = require("vite-tsconfig-paths").default;
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
@@ -11,5 +22,14 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  viteFinal: async (config) => {
+    // https://github.com/storybookjs/builder-vite/issues/85#issuecomment-1294339832
+    config.plugins.push(
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    );
+    return config;
   },
 };
