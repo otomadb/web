f2e9e29529180c9cd1e1607b12a62ab788eebd8f
SnO2WMaN <me@sno2wman.net>
2022-11-29 16:20:23 +0900

    uninstall recoil

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import React, { ReactNode } from "react";
-import { RecoilRoot } from "recoil";
+import { ReactNode } from "react";
 
 import { GraphQLProvider } from "~/hooks/useGraphQLClient";
 
-export const RecoilProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
-};
-
 export default function Providers({ children }: { children: ReactNode }) {
-  return (
-    <RecoilProvider>
-      <GraphQLProvider>{children}</GraphQLProvider>
-    </RecoilProvider>
-  );
+  return <GraphQLProvider>{children}</GraphQLProvider>;
 }
