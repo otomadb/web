d37cfdb7e45f96cd5f000cbad3c0ddc7a36a03be
SnO2WMaN <me@sno2wman.net>
2022-12-11 00:48:17 +0900

    chore: `img.cdn.nimg.jp`を画像取得先として許可

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["nicovideo.cdn.nimg.jp"],
+    domains: ["nicovideo.cdn.nimg.jp", "img.cdn.nimg.jp"],
   },
   experimental: {
     appDir: true,
