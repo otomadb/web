9fccdd58ab7e47562af8c13d905383a1688b6209
SnO₂WMaN <me@sno2wman.net>
2022-12-14 18:54:44 +0900

    chore: Storybookを動くようにした (#26)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,15 @@
-const path = require("path");
-const tsconfigPaths = require("vite-tsconfig-paths").default;
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
-      },
-    },
-    "storybook-addon-next",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
   },
-  features: {
-    storyStoreV7: true,
-  },
-  viteFinal: async (config) => {
-    // https://github.com/storybookjs/builder-vite/issues/85#issuecomment-1294339832
-    config.plugins.push(
-      tsconfigPaths({
-        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
-      })
-    );
-    return config;
+  docs: {
+    docsPage: "automatic",
   },
 };
