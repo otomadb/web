66ba822c4fc0e920b20b4101304fc847901cce3b
SnO2WMaN <me@sno2wman.net>
2022-12-16 11:59:01 +0900

    mgfx

--- src/app/videos/[id]/head.tsx
+++ src/app/videos/[id]/head.tsx
@@ -2,15 +2,13 @@ import gqlRequest from "graphql-request";
 
 import { CommonHead } from "~/app/CommonHead";
 import { graphql } from "~/gql";
-import { VideoPageTitleDocument } from "~/gql/graphql";
 
 export default async function Head({ params }: { params: { id: string } }) {
   const { video } = await gqlRequest(
     new URL("/graphql", process.env.NEXT_PUBLIC_API_ENDPOINT).toString(),
     graphql(`
-      query VideoPageTitle($id: ID!) {
+      query VideoPage_Title($id: ID!) {
         video(id: $id) {
-          id
           title
         }
       }
