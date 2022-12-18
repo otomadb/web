0fec9d28414586452493b3b5ecfee98a14a4d413
SnO2WMaN <me@sno2wman.net>
2022-12-18 12:01:35 +0900

    feat: Profileページはforce-dynamicに

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,8 @@ import { YouMustLogin } from "~/components/YouMustLogin";
 
 import { Profile } from "./Profile";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   return (
     <YouMustLogin>
