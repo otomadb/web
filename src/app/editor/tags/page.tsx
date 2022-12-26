7d7b8962650ee36303b599816d1e01382f54531e
SnO2WMaN <me@sno2wman.net>
2022-12-26 09:31:03 +0900

    pageNprovider

--- src/app/editor/tags/page.tsx
+++ src/app/editor/tags/page.tsx
@@ -4,7 +4,7 @@ import { RegisterTagForm } from "~/components/RegisterTag/Form";
 
 export default async function Page() {
   return (
-    <main className={clsx(["max-w-screen-xl"], ["mx-auto"])}>
+    <main className={clsx(["max-w-screen-lg"], ["mx-auto"])}>
       <h1>タグの追加</h1>
       <RegisterTagForm className={clsx(["mt-4"])} />
     </main>
