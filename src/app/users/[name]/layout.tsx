63a3a449b24495b2b7de3d0cad44156acf41d53f
SnO2WMaN <me@sno2wman.net>
2023-01-04 17:16:05 +0900

    Navをスタティックに

--- src/app/users/[name]/layout.tsx
+++ src/app/users/[name]/layout.tsx
@@ -5,10 +5,14 @@ import { notFound } from "next/navigation";
 import React from "react";
 
 import { getFragment, graphql } from "~/gql";
-import { UserPageLayout_HeaderFragmentDoc } from "~/gql/graphql";
+import {
+  UserPageLayout_HeaderFragmentDoc,
+  UserPageLayout_NavFragmentDoc,
+} from "~/gql/graphql";
 import { gqlRequest } from "~/utils/gqlRequest";
 
 import { Header } from "./Header";
+import { Nav } from "./Nav";
 
 export default async function Layout({
   children,
@@ -22,6 +26,7 @@ export default async function Layout({
       query UserPageLayout($name: String!) {
         findUser(input: { name: $name }) {
           ...UserPageLayout_Header
+          ...UserPageLayout_Nav
         }
       }
     `),
@@ -36,6 +41,10 @@ export default async function Layout({
         <Header
           className={clsx(["container", "max-w-screen-xl", "mx-auto"])}
           fragment={getFragment(UserPageLayout_HeaderFragmentDoc, findUser)}
+        />
+        <Nav
+          // highlight="MYLISTS"
+          user={getFragment(UserPageLayout_NavFragmentDoc, findUser)}
         />
         {children}
       </div>
