3846a87a07a1dda874b581751034450b37bca3f4
SnO2WMaN <me@sno2wman.net>
2023-01-04 16:00:46 +0900

    存在しないものは存在しない

--- src/app/profile/mylists/page.tsx
+++ src/app/profile/mylists/page.tsx
@@ -1,10 +1,7 @@
-import { Mylists } from "~/components/pages/UserMylists";
-
 export default async function Page() {
   return (
     <div>
       <h1>あなたのマイリスト一覧</h1>
-      <Mylists />
     </div>
   );
 }
