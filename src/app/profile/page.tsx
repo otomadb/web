20247c433c6cdc4696c8e15bd7eb5ca01f511c1e
SnO2WMaN <me@sno2wman.net>
2023-01-05 12:17:01 +0900

    応急処置

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,7 @@
 import { YouMustLogin } from "~/components/common/YouMustLogin";
-import { Profile } from "~/components/Profile/Profile";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  return (
-    <YouMustLogin>
-      <Profile />
-    </YouMustLogin>
-  );
+  return <YouMustLogin />;
 }
