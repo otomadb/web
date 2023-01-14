0d9ae04249e0b8bfd5b3544c0d7b8f20364478e2
SnO2WMaN <me@sno2wman.net>
2023-01-14 23:35:32 +0900

    feat: GlobalNavにEditorRoleで見れる項目を制限

--- src/components/global/Navigation/Profile/Accordion.stories.tsx
+++ src/components/global/Navigation/Profile/Accordion.stories.tsx
@@ -1,6 +1,8 @@
 import { css } from "@emotion/css";
 import { Meta, StoryObj } from "@storybook/react";
 
+import { aUser } from "~/gql/graphql";
+
 import { Accordion } from "./Accordion";
 
 export default {
@@ -9,15 +11,47 @@ export default {
     className: css`
       width: 256px;
     `,
-    user: {
+  },
+} as Meta<typeof Accordion>;
+
+export const Normal: StoryObj<typeof Accordion> = {
+  name: "通常ユーザ",
+  args: {
+    fragment: aUser({
       id: "1",
       name: "sno2wman",
       displayName: "SnO2WMaN",
       icon: "/storybook/512x512.png",
-    },
+      isEditor: false,
+      isAdministrator: false,
+    }),
   },
-} as Meta<typeof Accordion>;
+};
 
-export const Primary: StoryObj<typeof Accordion> = {
-  args: {},
+export const Editor: StoryObj<typeof Accordion> = {
+  name: "編集者",
+  args: {
+    fragment: aUser({
+      id: "1",
+      name: "sno2wman",
+      displayName: "SnO2WMaN",
+      icon: "/storybook/512x512.png",
+      isEditor: true,
+      isAdministrator: false,
+    }),
+  },
+};
+
+export const Adnimistrator: StoryObj<typeof Accordion> = {
+  name: "管理者",
+  args: {
+    fragment: aUser({
+      id: "1",
+      name: "sno2wman",
+      displayName: "SnO2WMaN",
+      icon: "/storybook/512x512.png",
+      isEditor: true,
+      isAdministrator: true,
+    }),
+  },
 };
