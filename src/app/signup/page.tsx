2c68ce68c1b5c521f6b8c9f557db821d2d42744e
SnO2WMaN <me@sno2wman.net>
2023-02-11 03:36:50 +0900

    Signup & Signin

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 import { SkipIfLoggedin } from "~/components/common/SkipIfLoggedin";
-import { SignupForm } from "~/components/Signup/SignupForm";
+import { SignupForm } from "~/components/pages/Signup/SignupForm";
 
 export default async function Page() {
   return (
