1e448f631a80e9f4c8ae5566d19c95c9dbc34df8
SnO2WMaN <me@sno2wman.net>
2023-02-15 22:32:47 +0900

    TagsSection tagging

--- src/components/pages/Video/TagsSection.server.tsx
+++ src/components/pages/Video/TagsSection.server.tsx
@@ -8,20 +8,25 @@ import { getFragment, graphql } from "~/gql";
 import {
   Component_TagFragmentDoc,
   VideoPage_TagsSectionFragment,
+  VideoPage_TagTypesListFragment,
+  VideoPage_TagTypesListFragmentDoc,
 } from "~/gql/graphql";
+import { styleByTagType } from "~/utils/styleByTagType";
 
 graphql(`
   fragment VideoPage_TagsSection on Video {
     id
-    tags(input: {}) {
-      tag {
-        id
-        ...Component_Tag
+    taggings(input: {}) {
+      ...VideoPage_TagTypesList
+      nodes {
+        tag {
+          id
+          ...Component_Tag
+        }
       }
     }
   }
 `);
-
 export const TagsSection: React.FC<{
   className?: string;
   fragment: VideoPage_TagsSectionFragment;
@@ -33,8 +38,14 @@ export const TagsSection: React.FC<{
           タグ
         </h2>
       </div>
+      <TagTypesList
+        fragment={getFragment(
+          VideoPage_TagTypesListFragmentDoc,
+          fragment.taggings
+        )}
+      />
       <div className={clsx(["mt-2"], ["flex", "flex-col", "items-start"])}>
-        {fragment.tags.map((tagging) => (
+        {fragment.taggings.nodes.map((tagging) => (
           <Tag
             key={tagging.tag.id}
             tag={getFragment(Component_TagFragmentDoc, tagging.tag)}
@@ -42,5 +53,40 @@ export const TagsSection: React.FC<{
         ))}
       </div>
     </section>
+  );
+};
+
+graphql(`
+  fragment VideoPage_TagTypesList on VideoTagConnection {
+    nodes {
+      tag {
+        pseudoType
+      }
+    }
+  }
+`);
+export const TagTypesList: React.FC<{
+  className?: string;
+  fragment: VideoPage_TagTypesListFragment;
+}> = ({ className, fragment }) => {
+  const types = fragment.nodes
+    .map(({ tag: { pseudoType } }) => pseudoType)
+    .filter((v1, i, arr) => i === arr.findIndex((v2) => v1 === v2));
+
+  return (
+    <div className={clsx(className, ["flex"], ["gap-x-2"], ["gap-y-2"])}>
+      {types.map((type) => (
+        <div key={type} className={clsx(["flex"])}>
+          <span
+            className={clsx(
+              ["select-all"],
+              ["text-xs", styleByTagType(type, "text")]
+            )}
+          >
+            {type}
+          </span>
+        </div>
+      ))}
+    </div>
   );
 };
