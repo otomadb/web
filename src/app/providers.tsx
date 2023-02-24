13637ba7a6eaf4535d3e9341b97dba2808ab800d
SnO2WMaN <me@sno2wman.net>
2023-02-24 10:47:13 +0900

    msw mock remove in devenv

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { cacheExchange } from "@urql/exchange-graphcache";
-import { setupWorker } from "msw";
 import { ReactNode } from "react";
 import {
   createClient as createUrqlClient,
@@ -12,30 +11,6 @@ import {
 
 import { GraphCacheConfig } from "~/gql/graphql";
 export const handlers = [];
-
-if (
-  process.env.NEXT_PUBLIC_MSW_ENABLE === "true" &&
-  typeof window !== "undefined"
-) {
-  const worker = setupWorker(...handlers);
-  worker.start({
-    onUnhandledRequest(request, print) {
-      if (
-        // prefetch
-        request.url.pathname === "/" ||
-        request.url.pathname.startsWith("/videos") ||
-        request.url.pathname.startsWith("/users") ||
-        request.url.pathname.startsWith("/tags") ||
-        // graphql
-        request.url.pathname === "/graphql" ||
-        // next
-        request.url.pathname.startsWith("/_next")
-      )
-        return;
-      print.warning();
-    },
-  });
-}
 
 const urqlClient = createUrqlClient({
   url: new URL("/graphql", process.env.NEXT_PUBLIC_API_ENDPOINT).toString(),
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,5 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_ENDPOINT: string;
-    NEXT_PUBLIC_MSW_ENABLE?: "true";
   }
 }
