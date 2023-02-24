65201b3dddec3e488539ec19a69d12232eafe92e
SnO2WMaN <me@sno2wman.net>
2023-02-24 10:42:28 +0900

    Add searchbox in toppage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import "server-only";
 import clsx from "clsx";
 import { Suspense } from "react";
 
+import { SearchBox } from "~/components/global/Navigation/SearchBox/SearchBox";
 import { NicovideoRequestsList } from "~/components/pages/Top/NicovideoRequestsList.server";
 import { RecentVideosList } from "~/components/pages/Top/RecentVIdeosList.server";
 
@@ -11,7 +12,20 @@ export default async function Page() {
     <main
       className={clsx(["container"], ["mx-auto"], ["flex", "gap-x-[12px]"])}
     >
-      <div className={clsx(["flex-shrink-0"], ["flex-grow"])}></div>
+      <div className={clsx(["flex-shrink-0"], ["flex-grow"])}>
+        <section
+          className={clsx(
+            [["px-2"], ["py-2"]],
+            ["rounded"],
+            ["border", "border-slate-300"]
+          )}
+        >
+          <h2 className={clsx(["text-sm"])}>検索</h2>
+          <div className={clsx(["mt-2"])}>
+            <SearchBox />
+          </div>
+        </section>
+      </div>
       <div
         className={clsx(
           ["flex-grow"],
