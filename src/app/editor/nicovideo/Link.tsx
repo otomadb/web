863e180c6de983531dd579ce7baa59282b1614c9
SnO₂WMaN <me@sno2wman.net>
2023-03-02 17:08:49 +0900

    feat: ニコニコ動画からのリクエストページに登録リンク追加 (#163)
    * feat: ニコニコ動画のリクエストページ作り直し
    
    * 登録するボタン追加
    
    * fix for `node:`
    
    * fix type

--- src/app/editor/nicovideo/Link.tsx
+++ src/app/editor/nicovideo/Link.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import { stringify } from "querystring";
+// import { stringify } from "node:querystring";
 import React, { ComponentProps } from "react";
 
+export type SearchParams = { sourceId?: string };
+
 export const LinkRegisterNicovideo: React.FC<
-  Omit<ComponentProps<typeof Link>, "href">
-> = ({ children, ...props }) => (
-  <Link href={"/editor/nicovideo"} {...props}>
+  Omit<ComponentProps<typeof Link>, "href"> & SearchParams
+> = ({ children, sourceId, ...props }) => (
+  <Link href={`/editor/nicovideo?${stringify({ sourceId })}`} {...props}>
     {children}
   </Link>
 );
