b07d36c2e9e3703ed1505a63e88d114165793c3d
SnO2WMaN <me@sno2wman.net>
2023-03-04 01:45:35 +0900

    Add tag

--- src/app/recent/videos/VideosGrid.tsx
+++ src/app/recent/videos/VideosGrid.tsx
@@ -2,12 +2,15 @@
 
 import { ResultOf } from "@graphql-typed-document-node/core";
 import clsx from "clsx";
+import Image from "next/image";
 import React from "react";
 import { useQuery } from "urql";
 
+import { LinkTag } from "~/app/tags/[serial]/Link";
 import { LinkVideo } from "~/app/videos/[serial]/Link";
 import { InfiniteVideosGrid } from "~/components/common/InfiniteVideoGrid";
 import { FetcherContainer } from "~/components/common/InfiniteVideoGrid";
+import { CommonTag } from "~/components/common/Tag";
 import { VideoThumbnail } from "~/components/common/VideoThumbnail";
 import { FragmentType, getFragment, graphql } from "~/gql";
 
@@ -58,6 +61,16 @@ const Fragment = graphql(`
     ...Link_Video
     ...VideoThumbnail
     title
+    thumbnailUrl
+    taggings(first: 3) {
+      nodes {
+        id
+        tag {
+          ...CommonTag
+          ...Link_Tag
+        }
+      }
+    }
   }
 `);
 const Video: React.FC<{
@@ -67,8 +80,32 @@ const Video: React.FC<{
   const fragment = getFragment(Fragment, props.fragment);
   return (
     <div
-      className={clsx(className, ["border"], ["rounded"], ["px-2"], ["py-2"])}
+      className={clsx(
+        className,
+        ["relative"],
+        ["overflow-hidden"],
+        ["rounded"],
+        ["border"],
+        ["px-2"],
+        ["py-2"],
+        ["flex", "flex-col"]
+      )}
     >
+      <Image
+        className={clsx(
+          ["-z-1"],
+          ["blur-sm", "scale-125", "contrast-75"],
+          ["opacity-25"],
+          ["absolute"],
+          ["inset-0"],
+          ["w-full", "h-full"],
+          ["object-cover"]
+        )}
+        src={fragment.thumbnailUrl}
+        width={512}
+        height={384}
+        alt={fragment.title}
+      />
       <LinkVideo className={clsx(["flex"])} fragment={fragment}>
         <VideoThumbnail
           fragment={fragment}
@@ -80,10 +117,38 @@ const Video: React.FC<{
       <div className={clsx(["mt-1"])}>
         <LinkVideo
           fragment={fragment}
-          className={clsx([["px-1"], ["py-1"]], ["text-xs"])}
+          className={clsx(["text-sm"], ["font-bold"], ["text-slate-900"])}
         >
           {fragment.title}
         </LinkVideo>
+      </div>
+      <div className={clsx(["mt-1"])}>
+        {fragment.taggings.nodes.length === 0 && (
+          <div className={clsx(["text-xxs"], ["text-slate-500"])}>
+            タグ付けがありません
+          </div>
+        )}
+        <div
+          className={clsx(
+            ["flex"],
+            ["flex-wrap"],
+            ["gap-x-0.5"],
+            ["gap-y-0.5"]
+          )}
+        >
+          {fragment.taggings.nodes.map((tagging) => (
+            <LinkTag
+              key={tagging.id}
+              fragment={tagging.tag}
+              className={clsx(["flex"])}
+            >
+              <CommonTag
+                fragment={tagging.tag}
+                className={clsx(["text-xxs"], ["px-1"], ["py-0.5"])}
+              />
+            </LinkTag>
+          ))}
+        </div>
       </div>
     </div>
   );
