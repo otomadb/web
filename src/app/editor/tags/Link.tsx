1913cefe11f2f61c1bdfb12abe28a528e8880f11
SnO2WMaN <me@sno2wman.net>
2023-03-15 20:32:37 +0900

    rename link

--- src/app/editor/tags/Link.tsx
+++ src/app/editor/tags/Link.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React, { ComponentProps } from "react";
 
-export const LinkRegisterTag: React.FC<
+export const TagRegisterPageLink: React.FC<
   Omit<ComponentProps<typeof Link>, "href">
 > = ({ children, ...props }) => (
   <Link href={"/editor/tags"} {...props}>
