11d0f9fe5a9b15045f9605b4c8c3d3950eb7169c
SnO2WMaN <me@sno2wman.net>
2023-03-17 05:31:43 +0900

    fix: `/you`のログイン検出削除

--- src/app/you/layout.tsx
+++ src/app/you/layout.tsx
@@ -1,17 +1,38 @@
 import clsx from "clsx";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
 import React from "react";
 
-import { YouPageGuard } from "./Guard";
+import { graphql } from "~/gql";
+import { fetchGql2 } from "~/gql/fetch";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  const session = cookieStore.get(process.env.SESSION_COOKIE_KEY)?.value;
+
+  const { whoami } = await fetchGql2(
+    {
+      document: graphql(`
+        query YouPage_Guard {
+          whoami {
+            id
+          }
+        }
+      `),
+      variables: {},
+    },
+    { session }
+  );
+  if (!whoami) return notFound();
+
   return (
     <div className={clsx(["h-full"])}>
       <div className={clsx(["container", "max-w-screen-xl", "mx-auto"])}>
-        <YouPageGuard>{children}</YouPageGuard>
+        {children}
       </div>
     </div>
   );
