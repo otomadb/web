0fb7b29e5542728ab3bba8cc153e2c62f5a72bbf
SnO₂WMaN <me@sno2wman.net>
2023-03-19 10:53:51 +0900

    fix: ニコニコ動画の登録及びリクエストページのURL組み立て修正 (#266)

--- src/app/editor/nicovideo/Link.tsx
+++ src/app/editor/nicovideo/Link.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import { stringify } from "querystring";
-// import { stringify } from "node:querystring";
 import React, { ComponentProps } from "react";
 
 export type SearchParams = { sourceId?: string };
@@ -8,7 +6,12 @@ export type SearchParams = { sourceId?: string };
 export const NicovideoRegisterPageLink: React.FC<
   Omit<ComponentProps<typeof Link>, "href"> & SearchParams
 > = ({ children, sourceId, ...props }) => (
-  <Link href={`/editor/nicovideo?${stringify({ sourceId })}`} {...props}>
+  <Link
+    href={
+      sourceId ? `/editor/nicovideo?sourceId=${sourceId}` : "/editor/nicovideo"
+    }
+    {...props}
+  >
     {children}
   </Link>
 );
