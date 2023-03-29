e9a6ed68855ce701377032511b434fc552dcf6e6
SnO2WMaN <me@sno2wman.net>
2023-03-30 00:46:55 +0900

    toaster

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -1,9 +1,96 @@
 "use client";
 
-import { createContext, ReactNode, useCallback, useContext } from "react";
+import { css, keyframes } from "@emotion/css";
+import clsx from "clsx";
+import {
+  ContextType,
+  createContext,
+  createRef,
+  forwardRef,
+  Fragment,
+  ReactNode,
+  RefObject,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { createPortal } from "react-dom";
+
+// eslint-disable-next-line react/display-name
+const Toast = forwardRef<
+  HTMLDivElement,
+  { children: ReactNode; fired: number; duration: number }
+>(({ children, fired, duration }, ref) => {
+  const [pausedAt, setPausedAt] = useState<number | undefined>(undefined);
+  const [until, setUntil] = useState(fired + duration);
+  const [hidden, setHidden] = useState(false);
+
+  const reqIdRef = useRef<number>();
+  const loop = useCallback(() => {
+    if (!pausedAt && until < Date.now()) setHidden(true);
+    reqIdRef.current = requestAnimationFrame(loop);
+  }, [pausedAt, until]);
+  useEffect(() => {
+    loop();
+    return () => {
+      if (reqIdRef.current) cancelAnimationFrame(reqIdRef.current);
+    };
+  }, [loop]);
+
+  return (
+    <div
+      ref={ref}
+      aria-hidden={hidden}
+      onMouseEnter={() => {
+        setPausedAt(Date.now());
+      }}
+      onMouseLeave={() => {
+        if (pausedAt) setUntil((prev) => prev + Date.now() - pausedAt);
+        setPausedAt(undefined);
+      }}
+      className={clsx(
+        ["relative"],
+        ["bg-white", "bg-opacity-75", "backdrop-blur"],
+        ["overflow"],
+        ["shadow"],
+        css(`
+          &[aria-hidden="true"] {
+            animation-duration: 0.25s;
+            animation-timing-function: ease-out;
+            animation-fill-mode: both;
+            animation-name: ${keyframes(
+              "from{visibility:visible; opacity:1;} to{visibility:hidden; opacity:0;}"
+            )};
+          }
+        `)
+      )}
+    >
+      <div className={clsx(["px-4", "py-2"])}>{children}</div>
+      <div
+        className={clsx(
+          ["w-full"],
+          ["h-[2px]"],
+          ["bg-teal-400"],
+          css(`
+            transform-origin: left;
+            animation-name: ${keyframes("from{scale: 1 1;} to{scale: 0 1;}")};
+            animation-fill-mode: both;
+            animation-timing-function: linear;
+          `)
+        )}
+        style={{
+          animationDuration: `${duration}ms`,
+          animationPlayState: pausedAt ? "paused" : "running",
+        }}
+      ></div>
+    </div>
+  );
+});
 
 export const ToastContext = createContext<{
-  call: (toast: JSX.Element) => void;
+  call: (inner: JSX.Element, option?: { duration: number }) => void;
 }>({
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   call: () => {},
@@ -11,11 +98,63 @@ export const ToastContext = createContext<{
 export const ToastProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const call = useCallback(() => {
-    console.log("TODO");
+  const [toasts, setToasts] = useState<
+    { id: string; ref: RefObject<HTMLDivElement>; Toast: JSX.Element }[]
+  >([]);
+
+  const reqIdRef = useRef<number>();
+  const loop = useCallback(() => {
+    setToasts((prev) =>
+      prev.filter(
+        ({ ref }) =>
+          !ref.current ||
+          window.getComputedStyle(ref.current, null).visibility !== "hidden"
+      )
+    );
+    reqIdRef.current = requestAnimationFrame(loop);
   }, []);
+  useEffect(() => {
+    loop();
+    return () => {
+      if (reqIdRef.current) cancelAnimationFrame(reqIdRef.current);
+    };
+  }, [loop]);
+
+  const call = useCallback((inner, { duration } = { duration: 3000 }) => {
+    const id = crypto.randomUUID();
+    const ref = createRef<HTMLDivElement>();
+    setToasts((prev) => [
+      ...prev,
+      {
+        id,
+        ref,
+        Toast: (
+          <Toast ref={ref} fired={Date.now()} duration={duration}>
+            {inner}
+          </Toast>
+        ),
+      },
+    ]);
+  }, []) satisfies ContextType<typeof ToastContext>["call"];
   return (
-    <ToastContext.Provider value={{ call }}>{children}</ToastContext.Provider>
+    <ToastContext.Provider value={{ call }}>
+      {children}
+      {createPortal(
+        <div
+          className={clsx(
+            ["w-96"],
+            ["fixed", "bottom-0", "right-0"],
+            ["flex", "flex-col", "flex-col-reverse", "gap-y-2"],
+            ["mr-4", "mb-4"]
+          )}
+        >
+          {toasts.map(({ id, Toast }) => (
+            <Fragment key={id}>{Toast}</Fragment>
+          ))}
+        </div>,
+        document.body
+      )}
+    </ToastContext.Provider>
   );
 };
 
