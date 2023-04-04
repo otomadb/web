e392828d3cf021c65e60680640e4e1f01104dc96
SnO2WMaN <me@sno2wman.net>
2023-04-04 19:47:25 +0900

    fx api

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,15 +2,8 @@ import { NextApiHandler } from "next";
 import httpProxyMiddleware from "next-http-proxy-middleware";
 
 const handler = (async (req, res) => {
-  /*
-  const accessToken = await getAccessToken(req, res)
-    .then(({ accessToken }) => accessToken)
-    .catch(() => null);
-
-  */
-
   return httpProxyMiddleware(req, res, {
-    target: process.env.GRAPHQL_API_ENDPOINT, // TODO: NEXT_PUBLICではない
+    target: process.env.GRAPHQL_API_ENDPOINT,
     proxyTimeout: 5000,
     pathRewrite: [{ patternStr: "^/api/graphql", replaceStr: "/graphql" }],
   });
