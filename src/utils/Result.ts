8933df83901a5b1920fb93417b196a40e042e932
SnO2WMaN <me@sno2wman.net>
2023-04-07 07:57:12 +0900

    fix misc

--- src/utils/Result.ts
+++ src/utils/Result.ts
@@ -1,17 +1,28 @@
 export const ok = <TData>(data: TData) => ({ status: "ok", data } as const);
-export type Ok<TResult> = TResult extends ReturnType<typeof ok<infer TData>> ? ReturnType<typeof ok<TData>> : never;
+export type Ok<TResult> = TResult extends ReturnType<typeof ok<infer TData>>
+  ? ReturnType<typeof ok<TData>>
+  : never;
 export type OkData<TResult> = Ok<TResult>["data"];
-export const isOk = <TError, TData>(res: Result<TError, TData>): res is Ok<Result<TError, TData>> =>
-  res.status === "ok";
-export type ReturnOk<TFn extends (...args: never) => unknown> = Ok<Awaited<ReturnType<TFn>>>;
+export const isOk = <TError, TData>(
+  res: Result<TError, TData>
+): res is Ok<Result<TError, TData>> => res.status === "ok";
+export type ReturnOk<TFn extends (...args: never) => unknown> = Ok<
+  Awaited<ReturnType<TFn>>
+>;
 
-export const err = <TError>(error: TError) => ({ status: "error", error } as const);
+export const err = <TError>(error: TError) =>
+  ({ status: "err", error } as const);
 export type Err<TResult> = TResult extends ReturnType<typeof err<infer TError>>
   ? ReturnType<typeof err<TError>>
   : never;
 export type ErrError<TResult> = Err<TResult>["error"];
-export const isErr = <TError, TData>(res: Result<TError, TData>): res is Err<Result<TError, TData>> =>
-  res.status === "error";
-export type ReturnErr<TFn extends (...args: never) => unknown> = Err<Awaited<ReturnType<TFn>>>;
+export const isErr = <TError, TData>(
+  res: Result<TError, TData>
+): res is Err<Result<TError, TData>> => res.status === "err";
+export type ReturnErr<TFn extends (...args: never) => unknown> = Err<
+  Awaited<ReturnType<TFn>>
+>;
 
-export type Result<TError, TData> = ReturnType<typeof err<TError>> | ReturnType<typeof ok<TData>>;
+export type Result<TError, TData> =
+  | ReturnType<typeof err<TError>>
+  | ReturnType<typeof ok<TData>>;
