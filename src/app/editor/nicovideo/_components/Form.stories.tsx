141c46036f61abae0ba0bcbafdf47a8c3dab0181
SnO2WMaN <me@sno2wman.net>
2023-04-09 14:55:56 +0900

    fxtp

--- src/app/editor/nicovideo/_components/Form.stories.tsx
+++ src/app/editor/nicovideo/_components/Form.stories.tsx
@@ -6,11 +6,9 @@ import {
   Provider as UrqlProvider,
 } from "urql";
 
-import { Fragment as CommonTagFragment } from "~/components/CommonTag";
 import { SourceIdContext } from "~/components/NicovideoSourceIdForm/SourceIdProvider";
-import { Query as SearchBoxQuery } from "~/components/TagSearcher/SearchBox";
-import { Fragment as SuggestItemFragment } from "~/components/TagSearcher/SuggestItem";
-import { Fragment as SuggestsFragment } from "~/components/TagSearcher/Suggests";
+import { mockTagSearcher } from "~/components/TagSearcher/index.mocks";
+import {} from "~/components/TagSearcher/SearchBox";
 import { makeFragmentData } from "~/gql";
 import {
   aNicovideoOriginalSource,
@@ -231,96 +229,7 @@ export const Primary: StoryObj<typeof meta> = {
             }
           }
         ),
-        graphql.query(SearchBoxQuery, (req, res, ctx) =>
-          res(
-            ctx.data({
-              searchTags: {
-                ...makeFragmentData(
-                  {
-                    items: [
-                      {
-                        ...makeFragmentData(
-                          {
-                            name: {
-                              id: "n1",
-                              primary: false,
-                              name: "ぼっち・ざ・まっど！",
-                            },
-                            tag: {
-                              id: "t1",
-                              ...makeFragmentData(
-                                {
-                                  name: "ぼっち・ざ・ろっく！",
-                                  type: TagType.Copyright,
-                                  explicitParent: null,
-                                },
-                                CommonTagFragment
-                              ),
-                            },
-                          },
-                          SuggestItemFragment
-                        ),
-                      },
-                      {
-                        ...makeFragmentData(
-                          {
-                            name: {
-                              id: "n2",
-                              primary: true,
-                              name: "後藤ひとり",
-                            },
-                            tag: {
-                              id: "t2",
-                              ...makeFragmentData(
-                                {
-                                  name: "後藤ひとり",
-                                  type: TagType.Character,
-                                  explicitParent: {
-                                    id: "t1",
-                                    name: "ぼっち・ざ・ろっく！",
-                                  },
-                                },
-                                CommonTagFragment
-                              ),
-                            },
-                          },
-                          SuggestItemFragment
-                        ),
-                      },
-                      {
-                        ...makeFragmentData(
-                          {
-                            name: {
-                              id: "n3",
-                              primary: true,
-                              name: "伊地知虹夏",
-                            },
-                            tag: {
-                              id: "t3",
-                              ...makeFragmentData(
-                                {
-                                  name: "伊地知虹夏",
-                                  type: TagType.Character,
-                                  explicitParent: {
-                                    id: "t1",
-                                    name: "ぼっち・ざ・ろっく！",
-                                  },
-                                },
-                                CommonTagFragment
-                              ),
-                            },
-                          },
-                          SuggestItemFragment
-                        ),
-                      },
-                    ],
-                  },
-                  SuggestsFragment
-                ),
-              },
-            })
-          )
-        ),
+        mockTagSearcher,
         graphql.mutation(
           RegisterNicovideoPage_RegisterForm_RegisterVideoDocument,
           (req, res, ctx) => {
