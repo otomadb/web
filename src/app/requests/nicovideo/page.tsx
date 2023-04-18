07f2587238c47e0c614e49fa79070acf8b0f6c0f
SnO2WMaN <me@sno2wman.net>
2023-04-18 13:57:18 +0900

    rq

--- src/app/requests/nicovideo/page.tsx
+++ src/app/requests/nicovideo/page.tsx
@@ -1,7 +1,12 @@
 import clsx from "clsx";
 
+import { graphql } from "~/gql";
+import { fetchGql } from "~/gql/fetch";
+import { isErr } from "~/utils/Result";
+
 import { SearchParams } from "./Link";
-import { VideoGrid } from "./VideosGrid";
+import Pagination from "./Pagination";
+import RequestsGrid from "./RequestsGrid";
 
 export const dynamic = "force-dynamic";
 
@@ -10,15 +15,64 @@ export default async function Page({
 }: {
   searchParams: SearchParams;
 }) {
+  const result = await fetchGql(
+    graphql(`
+      query AllNicovideoRequestsPage_After($first: Int!, $after: String) {
+        findNicovideoRegistrationRequests(
+          first: $first
+          after: $after
+          checked: false
+        ) {
+          totalCount
+          ...AllNicovideoRequestsPage_RequestsGrid
+          ...AllNicovideoRequestsPage_Pagination
+        }
+      }
+    `),
+    {
+      first: 30,
+      after: "after" in searchParams ? searchParams.after : null,
+    }
+  );
+  if (isErr(result)) throw new Error("Fetched failed");
+
   return (
-    <main className={clsx(["container"], ["mx-auto"], ["flex", "flex-col"])}>
-      <header>
-        <h1 className={clsx(["text-xl"])}>
-          最近リクエストされたニコニコ動画の動画
-        </h1>
-      </header>
-      <section className={clsx(["mt-4"], ["w-full"])}>
-        <VideoGrid initAfter={searchParams.after} />
+    <main
+      className={clsx(
+        ["@container"],
+        ["container"],
+        ["mx-auto"],
+        ["flex", "flex-col"]
+      )}
+    >
+      <div className={clsx(["flex"], ["w-full", "max-w-screen-lg", "mx-auto"])}>
+        <header className={clsx(["flex-grow"], ["py-4"])}>
+          <div>
+            <h1 className={clsx(["text-xl", "text-slate-900"])}>
+              最近リクエストされたニコニコ動画の動画
+            </h1>
+          </div>
+          <p className={clsx(["text-sm", "text-slate-500"])}>
+            {result.data.findNicovideoRegistrationRequests.totalCount}
+            件申請されています
+          </p>
+        </header>
+        <nav
+          className={clsx(
+            ["flex-shrink-0"],
+            ["sticky", "top-[64px]", "z-1"],
+            ["px-4", "py-4"]
+          )}
+        >
+          <Pagination
+            fragment={result.data.findNicovideoRegistrationRequests}
+          />
+        </nav>
+      </div>
+      <section className={clsx(["flex-grow"])}>
+        <RequestsGrid
+          fragment={result.data.findNicovideoRegistrationRequests}
+        />
       </section>
     </main>
   );
