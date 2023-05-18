30c8f2939b8ba15d48f4dd325090ed999fbb5702
SnO₂WMaN <me@sno2wman.net>
2023-05-18 16:07:31 +0900

    feat: `/developer`削除 (#510)

--- src/app/developer/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import "client-only";
-
-import DeveloperPagesGuard from "./Guard";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <DeveloperPagesGuard>{children}</DeveloperPagesGuard>;
-}
