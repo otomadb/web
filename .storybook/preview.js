7126497477e041b0330aa8619dac72e6a2ce5cb4
SnO2WMaN <me@sno2wman.net>
2023-09-19 06:39:15 +0900

    wip

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import "../src/app/globals.css";
-
+import { rest } from "msw";
+import { readFile } from "fs/promises";
 import { initialize as initializeMSW, mswDecorator } from "msw-storybook-addon";
 
 initializeMSW({
@@ -18,5 +19,23 @@ export const parameters = {
   },
   nextjs: {
     appDirectory: true,
+  },
+  parameters: {
+    msw: {
+      handlers: {
+        image: [
+          rest.get("/mads/:serial/thumbnail", async (req, res, ctx) => {
+            const imageBuffer = await readFile(
+              path.resolve(__dirname, "./public/800x450.jpg")
+            );
+            return res(
+              ctx.set("Cache-Control", "public, max-age=31536000, immutable"),
+              ctx.set("Content-Type", "image/jpeg"),
+              ctx.body(imageBuffer)
+            );
+          }),
+        ],
+      },
+    },
   },
 };
