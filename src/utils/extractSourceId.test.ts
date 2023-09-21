207a199eae8c9bfeb680549401b96f24eae8ea66
SnO₂WMaN <me@sno2wman.net>
2023-09-21 22:03:28 +0900

    入力時にYoutubeのURLを直接貼っても通るように (#756)

--- src/utils/extractSourceId.test.ts
+++ src/utils/extractSourceId.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, it } from "vitest";
 
-import { extractNicovideoSourceId } from "./extractSourceId";
+import {
+  extractNicovideoSourceId,
+  extractYoutubeSourceId,
+} from "./extractSourceId";
 
 describe("From Nicovideo", () => {
   it.each([
@@ -9,5 +12,15 @@ describe("From Nicovideo", () => {
     ["https://nico.ms/sm2057168", "sm2057168"],
   ])("%s => %s", (input, expected) => {
     expect(extractNicovideoSourceId(input)).toBe(expected);
+  });
+});
+
+describe("From Youtube", () => {
+  it.each([
+    ["Q16KpquGsIc", "Q16KpquGsIc"],
+    ["https://www.youtube.com/watch?v=Q16KpquGsIc", "Q16KpquGsIc"],
+    ["http://youtu.be/Q16KpquGsIc", "Q16KpquGsIc"],
+  ])("%s => %s", (input, expected) => {
+    expect(extractYoutubeSourceId(input)).toBe(expected);
   });
 });
