6942df124e5e3e5452e95f42d86fe1b15f27f36f
SnO₂WMaN <me@sno2wman.net>
2023-10-27 22:04:01 +0900

    `/me`ページのいいねした動画部分を改修 (#864)

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 
-import { YouLikesPageLink } from "./likes/Link";
 import RecentLikes from "./RecentLikes";
 import Timeline from "./Timeline";
 
@@ -10,28 +9,38 @@ export default async function Page() {
   return (
     <main
       className={clsx(
+        ["w-full", "max-w-screen-2xl"],
+        ["mx-auto"],
+        ["px-8"],
         ["py-4"],
-        ["container", "max-w-screen-2xl", "mx-auto"],
-        ["flex", "gap-x-4"]
+        ["@container/main"]
       )}
     >
-      <div className={clsx(["flex-grow"])}>
-        <Timeline />
-      </div>
-      <div className={clsx(["flex-shrink-0"], ["w-128"])}>
-        <section>
-          <div className={clsx(["flex", "items-center"], ["pr-4"])}>
-            <h2
-              className={clsx(["flex-grow"], ["text-md"], ["text-slate-900"])}
-            >
-              いいねした動画
-            </h2>
-            <YouLikesPageLink className={clsx(["text-xs"], ["text-slate-600"])}>
-              もっと見る
-            </YouLikesPageLink>
-          </div>
-          <RecentLikes className={clsx(["mt-2"])} />
-        </section>
+      <div
+        className={clsx([
+          "flex",
+          ["flex-col", "@w320/main:flex-row"],
+          "gap-x-4",
+          "gap-y-4",
+        ])}
+      >
+        <Timeline
+          className={clsx(
+            ["flex-grow"],
+            ["order-1", "@w320/main:order-0"],
+            ["w-full", "@w320/main:w-[1024px]"]
+          )}
+        />
+        <div
+          className={clsx(
+            ["flex-shrink-0"],
+            ["order-0", "@w320/main:order-1"],
+            ["flex", "flex-col"],
+            ["@w320/main:w-128"]
+          )}
+        >
+          <RecentLikes />
+        </div>
       </div>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    containers: {
+      w96: "24rem",
+      w128: "32rem",
+      w192: "48rem",
+      w240: "60rem",
+      w320: "80rem",
+    },
     extend: {
       fontSize: {
         xxs: ["0.6rem", "0.725rem"],
