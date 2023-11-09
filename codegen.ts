fbbfdc05d5a8926ff90875e00d26f6da0dc4b8ff
SnO2WMaN <me@sno2wman.net>
2023-11-10 00:16:36 +0900

    codegen fix

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "./schema.graphql",
-  documents: ["**/*.ts", "**/*.tsx", "**/*.graphql"],
+  documents: ["{app,components}/**/*.ts{,x}"],
   generates: {
     "gql/": {
       preset: "client",
