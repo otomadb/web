cea6be699b3646e3090bc04162391b701a09da9c
SnO2WMaN <me@sno2wman.net>
2023-11-26 04:09:02 +0900

    fixmore!

--- app/(application)/(normal)/layout.tsx
+++ app/(application)/(normal)/layout.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import "client-only";
+
+import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { ReactNode } from "react";
 
-function Layout({ children }: { children: ReactNode }) {
+export default withPageAuthRequired(function Layout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return <>{children}</>;
-}
-
-export default Layout;
+}, {});
--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,4 +1,4 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0/edge";
 
 export const GET = handleAuth({
   login: handleLogin({
