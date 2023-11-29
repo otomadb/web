3edcfec41f8c861615c07e0cea5e7a124b23c5f6
SnO2WMaN <me@sno2wman.net>
2023-11-30 07:53:18 +0900

    Application Sidebar wip
    Fixes #984

--- app/(v2)/layout.tsx
+++ app/(v2)/layout.tsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 import React from "react";
 
 import FormModal, { FormModalProvider } from "~/components/FormModal";
+import GlobalFooter from "~/components/GlobalFooter";
 import GlobalNav from "~/components/GlovalNav";
 
 export default function ApplicationLayout({
@@ -13,10 +14,12 @@ export default function ApplicationLayout({
   return (
     <UserProvider>
       <FormModalProvider>
-        <div className={clsx("bg-obsidian-darkest")}>
-          <GlobalNav className={clsx("sticky top-0 z-1 h-[64px] w-full")} />
-          <div className={clsx("flex min-h-[calc(100vh-64px)]")}>
-            <div className="shrink-0 grow">{children}</div>
+        <GlobalNav className={clsx("sticky top-0 z-1 h-[48px] w-full")} />
+        <div className={clsx("flex")}>
+          <div className="sticky top-[48px] h-[calc(100vh-48px)] w-[320px] shrink-0 border-r border-r-obsidian-lighter bg-obsidian-primary"></div>
+          <div className="shrink-0 grow bg-obsidian-darkest">
+            <div className="min-h-[calc(100vh-48px)] px-4 py-8">{children}</div>
+            <GlobalFooter />
           </div>
         </div>
         <FormModal className={clsx("fixed bottom-1 right-4")} />
