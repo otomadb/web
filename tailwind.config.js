cd4a4acbd35273dc388ce3ee6a7069b9eae9987d
SnO2WMaN <me@sno2wman.net>
2023-12-01 01:55:32 +0900

    TailwindCSSの設定を一部共通化

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const plugin = require("tailwindcss/plugin");
 const colors = require("tailwindcss/colors");
 
+const myConfig = require("@otomadb/tailwind-config");
+
 const mkTagTheme = (color) => ({
   primary: color[300],
   secondary: color[400],
@@ -54,23 +56,7 @@ module.exports = {
         50: "50ms",
       },
       colors: {
-        "vivid": {
-          primary: "hsl(162, 91%, 73%)",
-        },
-        "obsidian": {
-          darkest: "hsl(232, 34%, 6%)",
-          darker: "hsl(228, 21%, 9%)",
-          primary: "hsl(222, 17%, 13%)",
-          lighter: "hsl(218, 20%, 21%)",
-          lightest: "hsl(216, 13%, 28%)",
-        },
-        "snow": {
-          darkest: "hsl(210, 16%, 62%)",
-          darker: "hsl(210, 29%, 76%)",
-          primary: "hsl(210, 22%, 84%)",
-          lighter: "hsl(210, 18%, 92%)",
-          lightest: "hsl(210, 14%, 96%)",
-        },
+        ...myConfig.colors,
         // other services
         "nicovideo": { primary: "#FFF", sub: "#252525" },
         "youtube": { primary: "#FF0000" },
