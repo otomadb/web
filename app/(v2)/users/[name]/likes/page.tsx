775e36f3408d8d5e43f1c9d85652de58ecc2b3fa
SnO2WMaN <me@sno2wman.net>
2023-12-02 14:49:44 +0900

    componetinize

--- app/(v2)/users/[name]/likes/page.tsx
+++ app/(v2)/users/[name]/likes/page.tsx
@@ -1,13 +1,11 @@
 import clsx from "clsx";
 import { notFound } from "next/navigation";
 
-import { MadPageLink } from "~/app/(v2)/mads/[serial]/Link";
-import CommonTagLink from "~/components/CommonTagLink";
 import Paginator from "~/components/Paginator";
-import { VideoThumbnail } from "~/components/VideoThumbnail";
 import { graphql } from "~/gql";
 import { makeGraphQLClient } from "~/gql/fetch";
 
+import RegistrationItem from "../mylists/[slug]/RegistrationItem";
 import SideNav from "../SideNav";
 
 export default async function Page({
@@ -34,21 +32,7 @@ export default async function Page({
               totalCount
               nodes {
                 id
-                note
-                video {
-                  ...Link_Video
-                  ...VideoThumbnail
-                  id
-                  title
-                  taggings(first: 3) {
-                    nodes {
-                      id
-                      tag {
-                        ...CommonTagLink
-                      }
-                    }
-                  }
-                }
+                ...UserMylistPage_Registration
               }
             }
           }
@@ -63,9 +47,7 @@ export default async function Page({
   );
 
   const { findUser } = result;
-  if (!findUser || !findUser.likes) {
-    notFound();
-  }
+  if (!findUser || !findUser.likes) notFound();
 
   const pageMax = Math.ceil(
     findUser.likes.registrationsByOffset.totalCount / PER_PAGE
@@ -99,51 +81,8 @@ export default async function Page({
               "grid w-full grid-cols-1 flex-col gap-2 @[512px]/mylist:grid-cols-2 @[768px]/mylist:grid-cols-3 @[1024px]/mylist:grid-cols-4"
             )}
           >
-            {findUser.likes?.registrationsByOffset.nodes.map((node) => (
-              <div
-                key={node.id}
-                className={clsx(
-                  "group/registration flex flex-col rounded border border-obsidian-lighter bg-obsidian-primary @container/registration"
-                )}
-              >
-                <MadPageLink fragment={node.video} className={clsx("block")}>
-                  <VideoThumbnail
-                    fragment={node.video}
-                    className={clsx(
-                      "h-[108px] w-full shrink-0 border border-obsidian-lighter"
-                    )}
-                    imageSize="medium"
-                  />
-                </MadPageLink>
-                <div className={clsx("flex flex-col gap-y-2 px-4 py-2")}>
-                  <div>
-                    <MadPageLink
-                      fragment={node.video}
-                      className={clsx(
-                        "line-clamp-1 text-sm font-bold text-snow-primary hover:text-vivid-primary hover:underline"
-                      )}
-                    >
-                      {node.video.title}
-                    </MadPageLink>
-                  </div>
-                  <div className={clsx("")}>
-                    {node.video.taggings.nodes.length === 0 && (
-                      <div className={clsx("text-xxs text-slate-500")}>
-                        タグ付けがありません
-                      </div>
-                    )}
-                    <div className={clsx("flex flex-wrap gap-1")}>
-                      {node.video.taggings.nodes.map((tagging) => (
-                        <CommonTagLink
-                          size="xs"
-                          key={tagging.id}
-                          fragment={tagging.tag}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
+            {findUser.likes.registrationsByOffset.nodes.map((node) => (
+              <RegistrationItem key={node.id} fragment={node} />
             ))}
           </div>
           <div
--- app/(v2)/users/[name]/mylists/[slug]/page.tsx
+++ app/(v2)/users/[name]/mylists/[slug]/page.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
 import { notFound } from "next/navigation";
 
+import Paginator from "~/components/Paginator";
 import { graphql } from "~/gql";
 import { makeGraphQLClient } from "~/gql/fetch";
 
 import SideNav from "../../SideNav";
+import RegistrationItem from "./RegistrationItem";
 
 export default async function Page({
   params,
@@ -16,7 +18,7 @@ export default async function Page({
   const page = searchParams.page ? parseInt(searchParams.page, 10) : 1;
   if (page < 1) notFound();
 
-  const PER_PAGE = 12;
+  const PER_PAGE = 36;
 
   const result = await makeGraphQLClient().request(
     graphql(`
@@ -28,27 +30,15 @@ export default async function Page({
       ) {
         findUser(input: { name: $name }) {
           ...UserPage_SideNav
+          name
           mylist(id: $mylistId) {
-            id
+            slug
+            title
             registrationsByOffset(input: { offset: $offset, take: $take }) {
               totalCount
               nodes {
                 id
-                note
-                video {
-                  ...Link_Video
-                  ...VideoThumbnail
-                  id
-                  title
-                  taggings(first: 3) {
-                    nodes {
-                      id
-                      tag {
-                        ...CommonTag
-                      }
-                    }
-                  }
-                }
+                ...UserMylistPage_Registration
               }
             }
           }
@@ -63,8 +53,12 @@ export default async function Page({
     }
   );
 
-  if (!result.findUser) notFound();
   const { findUser } = result;
+  if (!findUser || !findUser.mylist) notFound();
+
+  const pageMax = Math.ceil(
+    findUser.mylist.registrationsByOffset.totalCount / PER_PAGE
+  );
 
   return (
     <div className={clsx("flex flex-wrap gap-x-4 @container/page")}>
@@ -74,7 +68,42 @@ export default async function Page({
           "grow border border-obsidian-primary bg-obsidian-darker p-4"
         )}
       >
-        <p className={clsx("text-snow-primary")}>ここにマイリストを表示する</p>
+        <div className={clsx("@container/mylist")}>
+          <div className={clsx("flex w-full items-center px-4 py-2")}>
+            <div className="shrink-0 grow">
+              <h1 className="text-xl font-bold text-snow-primary">
+                {findUser.mylist.title}が良いと思った音MAD
+              </h1>
+            </div>
+            <Paginator
+              size="sm"
+              className={clsx()}
+              pageMax={pageMax}
+              currentPage={page}
+              pathname={`/users/${findUser.name}/mylists/${findUser.mylist.slug}`}
+            />
+          </div>
+          <div
+            className={clsx(
+              "grid w-full grid-cols-1 flex-col gap-2 @[512px]/mylist:grid-cols-2 @[768px]/mylist:grid-cols-3 @[1024px]/mylist:grid-cols-4"
+            )}
+          >
+            {findUser.mylist.registrationsByOffset.nodes.map((node) => (
+              <RegistrationItem key={node.id} fragment={node} />
+            ))}
+          </div>
+          <div
+            className={clsx("flex w-full items-center justify-end px-4 py-2")}
+          >
+            <Paginator
+              size="sm"
+              className={clsx()}
+              pageMax={pageMax}
+              currentPage={page}
+              pathname={`/users/${findUser.name}/mylists/${findUser.mylist.slug}`}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
