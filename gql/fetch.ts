d6633973ac478b592413d13cd56474b3c54fa16d
SnO2WMaN <me@sno2wman.net>
2023-12-03 05:55:58 +0900

    `fetchGql`を削除
    Fixes #1023

--- gql/fetch.ts
+++ gql/fetch.ts
@@ -1,55 +1,7 @@
-// import "server-only";
+import "server-only";
 
-import { TypedDocumentNode } from "@graphql-typed-document-node/core";
-import { DocumentNode, Kind, print } from "graphql";
 import { GraphQLClient } from "graphql-request";
 import { RequestConfig } from "graphql-request/build/esm/types";
 
-import { err, ok, Result } from "~/utils/Result";
-
 export const makeGraphQLClient = (config: RequestConfig = {}) =>
   new GraphQLClient(process.env.GRAPHQL_API_ENDPOINT, config);
-
-/** Returns the name of the `DocumentNode`'s operation, if any.
- * @param query - A {@link DocumentNode}
- * @returns the operation's name contained within the document, or `undefined`
- * @see https://github.com/urql-graphql/urql/blob/21ccbad4e8090d72c3e93341fc4584ee7f475a7f/packages/core/src/utils/request.ts#L162-L172
- */
-const getOperationName = (query: DocumentNode): string | undefined => {
-  for (const node of query.definitions) {
-    if (node.kind === Kind.OPERATION_DEFINITION) {
-      return node.name ? node.name.value : undefined;
-    }
-  }
-};
-
-export const fetchGql = async <T, V extends Record<string, unknown>>(
-  document: TypedDocumentNode<T, V>,
-  variables: V,
-  init?: Omit<RequestInit, "body" | "method">
-): Promise<
-  Result<
-    | { type: "FETCH_ERROR"; status: number }
-    | { type: "GRAPHQL_ERROR"; errors: unknown },
-    T
-  >
-> =>
-  fetch(process.env.GRAPHQL_API_ENDPOINT, {
-    ...init,
-    method: "POST",
-    headers: {
-      ...init?.headers,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: print(document),
-      variables,
-      operationName: getOperationName(document),
-    }),
-  }).then(async (res) => {
-    if (!res.ok) return err({ type: "FETCH_ERROR", status: res.status });
-
-    const { errors, data } = await res.json();
-    if (errors) return err({ type: "GRAPHQL_ERROR", errors });
-    return ok(data as T);
-  });
