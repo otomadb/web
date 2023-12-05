c3cf046d291d32682b6cb87eb8c7c49230cf8f62
SnO₂WMaN <me@sno2wman.net>
2023-12-05 17:43:15 +0900

    ProfileAccordion の不要なものを削除 (#1103)

--- components/GlovalNav/ProfileAccordion.tsx
+++ components/GlovalNav/ProfileAccordion.tsx
@@ -3,31 +3,18 @@
 import clsx from "clsx";
 import React from "react";
 
-import { LinkRegisterSemitag } from "~/app/(application)/(editor)/editor/semitags/Link";
-import { TagRegisterPageLink } from "~/app/(application)/(editor)/editor/tags/Link";
 import { SettingPageLink } from "~/app/(application)/(normal)/settings/Link";
 import MyLikesPageLink from "~/app/(v2)/(authenticated)/me/likes/Link";
 import MyTopPageLink from "~/app/(v2)/(authenticated)/me/Link";
 import MyMylistsPageLink from "~/app/(v2)/(authenticated)/me/mylists/Link";
 import NotificationsPageLink from "~/app/(v2)/(authenticated)/notifications/Link";
 import { LogoutLink } from "~/components/AuthLink";
-import {
-  useOpenRegisterFromBilibili,
-  useOpenRegisterFromNicovideo,
-  useOpenRegisterFromYoutube,
-  useOpenRequestFromNicovideo,
-  useOpenRequestFromSoundcloud,
-  useOpenRequestFromYoutube,
-  useOpenSoundcloudRegisterModal,
-} from "~/components/FormModal";
 import { FragmentType, graphql, useFragment } from "~/gql";
 
 export const AccordionFragment = graphql(`
   fragment GlobalNav_ProfileAccordion on User {
     name
     displayName
-    isEditor: hasRole(role: EDITOR)
-    isAdmin: hasRole(role: ADMIN)
     notifications(input: { filter: { watched: false } }) {
       totalCount
     }
@@ -43,16 +30,7 @@ export default function ProfileAccordion({
   fragment: FragmentType<typeof AccordionFragment>;
 }) {
   const fragment = useFragment(AccordionFragment, props.fragment);
-  const { notifications, displayName, name, isEditor } = fragment;
-  const openRequestFromNicovideo = useOpenRequestFromNicovideo();
-
-  const openRegisterFromNicovideo = useOpenRegisterFromNicovideo();
-  const openRegisterFromYoutube = useOpenRegisterFromYoutube();
-  const openRegisterFromBilibili = useOpenRegisterFromBilibili();
-  const openSoundcloudRegisterModal = useOpenSoundcloudRegisterModal();
-
-  const openRequestFromYoutube = useOpenRequestFromYoutube();
-  const openRequestFromSoundcloud = useOpenRequestFromSoundcloud();
+  const { notifications, displayName, name } = fragment;
 
   return (
     <div
@@ -120,42 +98,7 @@ export default function ProfileAccordion({
         >
           マイリスト
         </MyMylistsPageLink>
-        <button
-          type="button"
-          onClick={(e) => {
-            e.currentTarget.blur();
-            openRequestFromNicovideo(null);
-          }}
-          className={clsx(
-            "block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-          )}
-        >
-          ニコニコ動画からリクエスト
-        </button>
-        <button
-          type="button"
-          onClick={(e) => {
-            e.currentTarget.blur();
-            openRequestFromYoutube(null);
-          }}
-          className={clsx(
-            "block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-          )}
-        >
-          YouTubeからリクエスト
-        </button>
-        <button
-          type="button"
-          onClick={(e) => {
-            e.currentTarget.blur();
-            openRequestFromSoundcloud(null);
-          }}
-          className={clsx(
-            "block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-          )}
-        >
-          Soundcloudからリクエスト
-        </button>
+
         <SettingPageLink
           className={clsx(
             "block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
@@ -164,72 +107,7 @@ export default function ProfileAccordion({
           設定
         </SettingPageLink>
       </div>
-      {isEditor && (
-        <div className={clsx("grid grid-cols-2")}>
-          <button
-            type="button"
-            onClick={(e) => {
-              e.currentTarget.blur();
-              openRegisterFromNicovideo(null);
-            }}
-            className={clsx(
-              "col-span-2 block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-            )}
-          >
-            ニコニコ動画から登録
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              e.currentTarget.blur();
-              openRegisterFromYoutube(null);
-            }}
-            className={clsx(
-              "col-span-2 block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-            )}
-          >
-            Youtubeから登録
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              e.currentTarget.blur();
-              openRegisterFromBilibili(null);
-            }}
-            className={clsx(
-              "col-span-2 block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-            )}
-          >
-            ビリビリ動画から登録
-          </button>
-          <button
-            type="button"
-            onClick={(e) => {
-              e.currentTarget.blur();
-              openSoundcloudRegisterModal(null);
-            }}
-            className={clsx(
-              "col-span-2 block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-            )}
-          >
-            Soundcloudから登録
-          </button>
-          <TagRegisterPageLink
-            className={clsx(
-              "col-span-1 block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-            )}
-          >
-            タグの登録
-          </TagRegisterPageLink>
-          <LinkRegisterSemitag
-            className={clsx(
-              "col-span-1 block px-4 py-2 text-left text-xs text-snow-primary hover:bg-vivid-primary hover:text-obsidian-primary"
-            )}
-          >
-            仮タグの解決
-          </LinkRegisterSemitag>
-        </div>
-      )}
+
       <LogoutLink
         className={clsx(
           "px-4 py-2 text-xs text-snow-darkest hover:bg-vivid-primary hover:text-obsidian-primary"
