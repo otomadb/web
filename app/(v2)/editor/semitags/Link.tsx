5d65eeab3e659ebcbf0cce2abcb9ec41a51e7dac
SnO2WMaN <me@sno2wman.net>
2023-12-13 08:52:54 +0900

    とりあえず一時的に`/editor/{tags,semitags}`をv2へ

--- app/(v2)/editor/semitags/Link.tsx
+++ app/(v2)/editor/semitags/Link.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React, { ComponentProps } from "react";
 
-export const LinkRegisterSemitag: React.FC<
+const LinkRegisterSemitag: React.FC<
   Omit<ComponentProps<typeof Link>, "href">
 > = ({ children, ...props }) => (
   <Link href={"/editor/semitags"} {...props}>
     {children}
   </Link>
 );
+export default LinkRegisterSemitag;
--- app/(v2)/editor/tags/Link.tsx
+++ app/(v2)/editor/tags/Link.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React, { ComponentProps } from "react";
 
-export const TagRegisterPageLink: React.FC<
+const TagRegisterPageLink: React.FC<
   Omit<ComponentProps<typeof Link>, "href">
 > = ({ children, ...props }) => (
   <Link href={"/editor/tags"} {...props}>
     {children}
   </Link>
 );
+export default TagRegisterPageLink;
