f316d04da49a8fc28aa0bd3d70a672d390e10b06
SnO2WMaN <me@sno2wman.net>
2023-12-12 23:41:31 +0900

    mock `next/navigation`

--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,23 @@
 import "@testing-library/jest-dom/vitest";
 
 import { setProjectAnnotations } from "@storybook/react";
+import { vi } from "vitest";
 
 import preview from "../.storybook/preview";
+
+const { useRouter } = vi.hoisted(() => {
+  const mockedRouterPush = vi.fn();
+  return {
+    useRouter: () => ({ push: mockedRouterPush }),
+  };
+});
+
+vi.mock("next/navigation", async () => {
+  const actual = await vi.importActual("next/navigation");
+  return {
+    ...actual,
+    useRouter,
+  };
+});
 
 setProjectAnnotations(preview);
