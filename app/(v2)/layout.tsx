99c5a0cfdd848cad285f2b3d1ac50e3262480808
SnO2WMaN <me@sno2wman.net>
2023-12-13 20:22:47 +0900

    part

--- app/(v2)/layout.tsx
+++ app/(v2)/layout.tsx
@@ -3,13 +3,38 @@ import React, { Suspense } from "react";
 
 import FormWidgetSwitch, { FormModalProvider } from "~/components/FormWidget";
 import GlobalFooter from "~/components/GlobalFooter";
-import GlobalNav from "~/components/GlobalNav/GlobalNav";
+import Logo from "~/components/Logo";
+import SearchContents from "~/components/SearchContents";
 import { ToastProvider } from "~/components/Toaster";
 import { graphql } from "~/gql";
 import { makeGraphQLClient2 } from "~/gql/fetch";
 
+import TopPageLink from "../(landing)/Link";
 import AppSideNavMenu from "./AppSideNavMenu";
+import GlobalNavUserPart from "./GlobalNavUserPart";
 import { NotifyEmailVerifycation } from "./NotifyVerification";
+
+const getGlobalNavUserPart = async () =>
+  makeGraphQLClient2({ auth: "optional" }).then((c) =>
+    c.request(
+      graphql(`
+        query GlobalNav {
+          ...GlobalNav
+        }
+      `)
+    )
+  );
+
+const getAppSideNav = async () =>
+  makeGraphQLClient2({ auth: "optional" }).then((c) =>
+    c.request(
+      graphql(`
+        query AppSideNav {
+          ...AppSideNav
+        }
+      `)
+    )
+  );
 
 export default async function ApplicationLayout({
   children,
@@ -20,20 +45,44 @@ export default async function ApplicationLayout({
     <ToastProvider selector="#toast2">
       <NotifyEmailVerifycation />
       <FormModalProvider>
-        <GlobalNav className={"sticky top-0 z-1 h-[64px] w-full"} />
-        <div className="flex bg-obsidian-darkest">
+        <nav
+          className={clsx(
+            "sticky top-0 z-1 h-[64px] w-full border-b border-b-obsidian-primary bg-obsidian-darker/80 backdrop-blur-md"
+          )}
+        >
+          <div
+            className={clsx(
+              "mx-auto flex h-full max-w-[1280px] items-center justify-between gap-x-8 px-8"
+            )}
+          >
+            <TopPageLink
+              className={clsx(
+                "block h-[24px] shrink-0 fill-snow-darkest transition-colors duration-200 hover:fill-vivid-primary"
+              )}
+            >
+              <Logo className={clsx("h-full")} />
+            </TopPageLink>
+            <SearchContents className={clsx("z-0 grow")} />
+            <div className={clsx("w-[128px] shrink-0")}>
+              <Suspense
+                fallback={
+                  <div
+                    className={
+                      "h-[32px] w-[32px] animate-pulse rounded-sm bg-obsidian-lighter"
+                    }
+                  />
+                }
+              >
+                <GlobalNavUserPart fragment={await getGlobalNavUserPart()} />
+              </Suspense>
+            </div>
+          </div>
+        </nav>
+        <div className="relative z-0 flex bg-obsidian-darkest">
           <nav className="sticky top-[64px] h-[calc(100vh-64px)] w-[320px] shrink-0 border-r border-obsidian-primary bg-obsidian-darker">
             <Suspense>
               <AppSideNavMenu
-                fragment={await (
-                  await makeGraphQLClient2({ auth: "optional" })
-                ).request(
-                  graphql(`
-                    query AppSideNav {
-                      ...AppSideNav
-                    }
-                  `)
-                )}
+                fragment={await getAppSideNav()}
                 className={clsx("h-full w-full")}
               />
             </Suspense>
