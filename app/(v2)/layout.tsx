8722c0539a1f25c04d4b6e430938bb8ce51ae5da
SnO₂WMaN <me@sno2wman.net>
2023-12-13 21:40:26 +0900

    ナビゲーション周りを改修 (#1227)

--- app/(v2)/layout.tsx
+++ app/(v2)/layout.tsx
@@ -1,42 +1,111 @@
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 import clsx from "clsx";
-import React from "react";
+import React, { Suspense } from "react";
 
-import AppSideNav from "~/components/AppSideNav";
 import FormWidgetSwitch, { FormModalProvider } from "~/components/FormWidget";
 import GlobalFooter from "~/components/GlobalFooter";
-import GlobalNav from "~/components/GlovalNav";
+import Logo from "~/components/Logo";
+import SearchContents from "~/components/SearchContents";
 import { ToastProvider } from "~/components/Toaster";
+import { graphql } from "~/gql";
+import { makeGraphQLClient2 } from "~/gql/fetch";
 
+import TopPageLink from "../(landing)/Link";
+import AppSideNavMenu from "./AppSideNavMenu";
+import GlobalNavUserPart from "./GlobalNavUserPart";
 import { NotifyEmailVerifycation } from "./NotifyVerification";
 
-export default function ApplicationLayout({
+const getGlobalNavUserPart = async () =>
+  makeGraphQLClient2({ auth: "optional" }).then((c) =>
+    c.request(
+      graphql(`
+        query GlobalNav {
+          ...GlobalNav
+        }
+      `)
+    )
+  );
+
+const getAppSideNav = async () =>
+  makeGraphQLClient2({ auth: "optional" }).then((c) =>
+    c.request(
+      graphql(`
+        query AppSideNav {
+          ...AppSideNav
+        }
+      `)
+    )
+  );
+
+export default async function ApplicationLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <UserProvider>
-      <ToastProvider selector="#toast2">
-        <NotifyEmailVerifycation />
-        <FormModalProvider>
-          <div className={clsx("flex @container/app")}>
-            <AppSideNav className="sticky top-0 h-screen w-[320px] shrink-0" />
-            <div className="shrink-0 grow bg-obsidian-darkest">
-              <GlobalNav className={clsx("sticky top-0 z-1 h-[64px] w-full")} />
-              <div className="min-h-[calc(100vh-64px)]">{children}</div>
-              <GlobalFooter />
+    <ToastProvider selector="#toast2">
+      <NotifyEmailVerifycation />
+      <FormModalProvider>
+        <div className={clsx("flex @container/app")}>
+          <nav className="sticky top-0 h-screen w-[320px] shrink-0 border-r border-obsidian-primary bg-obsidian-darker">
+            <div
+              className={clsx(
+                "flex h-[64px] items-center justify-center border-b border-obsidian-primary"
+              )}
+            >
+              <TopPageLink
+                className={clsx(
+                  "border-b border-obsidian-primary fill-snow-darkest transition-colors duration-50 hover:fill-vivid-primary"
+                )}
+              >
+                <Logo className={clsx("mx-auto h-[32px]")} />
+              </TopPageLink>
             </div>
+            <Suspense>
+              <AppSideNavMenu
+                fragment={await getAppSideNav()}
+                className={clsx("h-full w-full")}
+              />
+            </Suspense>
+          </nav>
+          <div className="relative z-0 grow bg-obsidian-darkest">
+            <nav
+              className={clsx(
+                "sticky top-0 z-1 h-[64px] w-full border-b border-b-obsidian-primary bg-obsidian-darker/80 backdrop-blur-md"
+              )}
+            >
+              <div
+                className={clsx(
+                  "mx-auto flex h-full max-w-[1280px] items-center justify-between gap-x-8 px-8"
+                )}
+              >
+                <SearchContents className={clsx("z-0 grow")} />
+                <div className={clsx("w-[128px] shrink-0")}>
+                  <Suspense
+                    fallback={
+                      <div
+                        className={
+                          "h-[32px] w-[32px] animate-pulse rounded-sm bg-obsidian-lighter"
+                        }
+                      />
+                    }
+                  >
+                    <GlobalNavUserPart
+                      fragment={await getGlobalNavUserPart()}
+                    />
+                  </Suspense>
+                </div>
+              </div>
+            </nav>
+            <div className="min-h-[calc(100vh-64px)]">{children}</div>
+            <GlobalFooter />
           </div>
-          <FormWidgetSwitch className={clsx("fixed bottom-1 right-4")} />
-          <div
-            id="toast2"
-            className={clsx(
-              "fixed right-4 top-[64px] z-infinity max-h-[calc(100vh-64px)] min-w-[320px]"
-            )}
-          />
-        </FormModalProvider>
-      </ToastProvider>
-    </UserProvider>
+        </div>
+        <FormWidgetSwitch className={"fixed bottom-1 right-4"} />
+        <div
+          id="toast2"
+          className="fixed right-4 top-[64px] z-infinity max-h-[calc(100vh-64px)] min-w-[320px]"
+        />
+      </FormModalProvider>
+    </ToastProvider>
   );
 }
