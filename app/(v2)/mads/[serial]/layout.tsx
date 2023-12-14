1120a5dcdd9cd29524e7f97c5cbcce309a42bcd9
SnO2WMaN <me@sno2wman.net>
2023-12-15 01:59:52 +0900

    pgfix

--- app/(v2)/mads/[serial]/layout.tsx
+++ app/(v2)/mads/[serial]/layout.tsx
@@ -116,184 +116,223 @@ export default async function Layout({
         "flex w-full flex-col gap-x-4 px-8 py-4 @container/layout"
       )}
     >
+      <header className={clsx("col-span-full p-4 @[1024px]/layout:p-8")}>
+        <h1 className={clsx("text-2xl font-bold text-snow-primary")}>
+          <MadPageLink fragment={video}>{video.title}</MadPageLink>
+        </h1>
+      </header>
       <div
         className={clsx(
-          "mx-auto flex w-full max-w-[1536px] flex-wrap items-start gap-x-2"
-        )}
-      >
-        <div className={clsx("grow py-4")}>
-          <VideoThumbnail
-            fragment={video}
-            imageSize="large"
-            className={clsx("mx-auto h-full max-h-[540px] max-w-[960px]")}
-          />
-        </div>
-        <div
-          className={clsx(
-            "grid w-full shrink-0  gap-x-1 gap-y-0.5",
-            "@[1024px]/layout:w-[256px] @[1024px]/layout:grid-cols-1",
-            "grid-cols-3"
-          )}
-        >
-          {nicovideoSources.map((source) => (
-            <div
-              key={source.id}
-              className={clsx(
-                "flex items-center gap-x-2 border-l-2 border-l-nicovideo-primary"
-              )}
-            >
-              <Link
-                href={{
-                  pathname: `/videos/${serial}`,
-                  query: { platform: "nicovideo", sourceId: source.sourceId },
-                }}
-                className={clsx(
-                  "group flex grow items-center gap-x-2 p-4 hover:bg-obsidian-lighter"
-                )}
-              >
-                <NicovideoPictogram
-                  className={clsx(
-                    "h-4 w-4 text-snow-darker group-hover:text-snow-lightest"
-                  )}
-                />
-                <div
-                  className={clsx(
-                    "font-mono text-xs text-snow-primary group-hover:text-snow-lightest"
-                  )}
-                >
-                  {source.sourceId}
-                </div>
-              </Link>
-            </div>
-          ))}
-          {youtubeSources.map((source) => (
-            <div
-              key={source.id}
-              className={clsx(
-                "flex items-center gap-x-2 border-l-2 border-l-youtube-primary"
-              )}
-            >
-              <Link
-                href={{
-                  pathname: `/videos/${serial}`,
-                  query: { platform: "youtube", sourceId: source.sourceId },
-                }}
-                className={clsx(
-                  "group flex grow items-center gap-x-2 p-4 hover:bg-obsidian-lighter"
-                )}
-              >
-                <YoutubePictogram
-                  className={clsx(
-                    "h-4 w-4 text-snow-darker group-hover:text-snow-lightest"
-                  )}
-                />
-                <div
-                  className={clsx(
-                    "font-mono text-xs text-snow-primary group-hover:text-snow-lightest"
-                  )}
-                >
-                  {source.sourceId}
-                </div>
-              </Link>
-            </div>
-          ))}
-          {bilibiliSources.map((source) => (
-            <div
-              key={source.id}
-              className={clsx(
-                "flex items-center gap-x-2 border-l-2 border-l-bilibili-primary"
-              )}
-            >
-              <Link
-                href={{
-                  pathname: `/videos/${serial}`,
-                  query: { platform: "bilibili", sourceId: source.sourceId },
-                }}
-                className={clsx(
-                  "group flex grow items-center gap-x-2 p-4 hover:bg-obsidian-lighter"
-                )}
-              >
-                <BilibiliPictogram
-                  className={clsx(
-                    "h-4 w-4 text-snow-darker group-hover:text-snow-lightest"
-                  )}
-                />
-                <div
-                  className={clsx(
-                    "font-mono text-xs text-snow-primary group-hover:text-snow-lightest"
-                  )}
-                >
-                  {source.sourceId}
-                </div>
-              </Link>
-            </div>
-          ))}
-          {soundcloudSources.map((source) => (
-            <div
-              key={source.id}
-              className={clsx(
-                "flex items-center gap-x-2 border-l-2 border-l-soundcloud-primary"
-              )}
-            >
-              <Link
-                href={{
-                  pathname: `/videos/${serial}`,
-                  query: { platform: "soundcloud", sourceId: source.sourceId },
-                }}
-                className={clsx(
-                  "group flex grow items-center gap-x-2 p-4 hover:bg-obsidian-lighter"
-                )}
-              >
-                <SoundcloudPictogram
-                  className={clsx(
-                    "h-4 w-4 text-snow-darker group-hover:text-snow-lightest"
-                  )}
-                />
-                <div
-                  className={clsx(
-                    "font-mono text-xs text-snow-primary group-hover:text-snow-lightest"
-                  )}
-                >
-                  {source.sourceId}
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div
-        className={clsx(
-          "mx-auto mt-4 flex w-full max-w-[1536px] gap-4",
+          "z-1 mx-auto mt-4 flex w-full max-w-[1536px] gap-4",
           "flex-col @[1024px]/layout:flex-row"
         )}
       >
-        <div
+        <nav
           className={clsx(
-            "flex w-full shrink-0 items-center justify-between",
-            "@[1024px]/layout:w-72 @[1024px]/layout:flex-col @[1024px]/layout:items-start @[1024px]/layout:justify-start"
+            "flex w-full shrink-0 flex-row items-stretch gap-x-2",
+            "@[1024px]/layout:w-72 @[1024px]/layout:flex-col @[1024px]/layout:items-start @[1024px]/layout:justify-start @[1024px]/layout:gap-y-4"
           )}
         >
-          <LikeButton
+          <div className={clsx("col-span-1 @[1024px]/layout:w-full")}>
+            <LikeButton
+              fragment={video}
+              className={clsx(
+                "w-32 @[1024px]/layout:w-full",
+                "shrink-0 @[1024px]/layout:max-w-none"
+              )}
+            />
+          </div>
+          <LinkVideoEvents
             fragment={video}
             className={clsx(
-              "w-full max-w-[256px] shrink-0 @[1024px]/layout:max-w-none"
+              "flex items-center justify-center px-4 text-sm text-snow-darker @[1024px]/layout:px-0 hover:bg-obsidian-lighter @[1024px]/layout:hover:bg-transparent",
+              "@[1024px]/layout:hover:text-vivid-primary @[1024px]/layout:hover:underline"
             )}
-          />
-          <div className={clsx()}>
-            <LinkVideoEvents
-              fragment={video}
-              className={clsx("text-sm text-snow-darker")}
+          >
+            編集履歴を見る
+          </LinkVideoEvents>
+          <div
+            className={clsx(
+              "group/sources relative flex items-center justify-center @[1024px]/layout:w-full",
+              "px-4 @[1024px]/layer:px-0",
+              "hover:bg-obsidian-lighter @[1024px]/layout:hover:bg-transparent"
+            )}
+          >
+            <div
+              className={clsx(
+                "text-sm text-snow-darker @[1024px]/layout:hidden"
+              )}
             >
-              編集履歴を見る
-            </LinkVideoEvents>
+              動画ソース
+            </div>
+            <div
+              className={clsx(
+                "left-0 top-full z-1 flex flex-col @[1024px]/layout:top-auto @[1024px]/layout:gap-y-2",
+                "w-64 @[1024px]/layout:w-full",
+                "absolute @[1024px]/layout:relative",
+                "invisible group-hover/sources:visible @[1024px]/layout:visible",
+                "divide-y divide-obsidian-lighter @[1024px]/layout:divide-y-0",
+                "border border-obsidian-lighter @[1024px]/layout:border-0",
+                "bg-obsidian-primary/90 backdrop-blur-md @[1024px]/layout:bg-transparent"
+              )}
+            >
+              {nicovideoSources.map((source) => (
+                <div
+                  key={source.id}
+                  className={clsx(
+                    "flex items-center gap-x-2 border-l-2 !border-l-nicovideo-primary"
+                  )}
+                >
+                  <Link
+                    href={{
+                      pathname: `/videos/${serial}`,
+                      query: {
+                        platform: "nicovideo",
+                        sourceId: source.sourceId,
+                      },
+                    }}
+                    className={clsx(
+                      "group/source flex grow items-center gap-x-2 hover:bg-vivid-primary @[1024px]/layout:hover:bg-obsidian-lighter",
+                      "p-4 @[1024px]/layout:py-2"
+                    )}
+                  >
+                    <NicovideoPictogram
+                      className={clsx(
+                        "h-4 w-4 text-snow-darker group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    />
+                    <div
+                      className={clsx(
+                        "font-mono text-xs text-snow-primary group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    >
+                      {source.sourceId}
+                    </div>
+                  </Link>
+                </div>
+              ))}
+              {youtubeSources.map((source) => (
+                <div
+                  key={source.id}
+                  className={clsx(
+                    "flex items-center gap-x-2 border-l-2 !border-l-youtube-primary"
+                  )}
+                >
+                  <Link
+                    href={{
+                      pathname: `/videos/${serial}`,
+                      query: { platform: "youtube", sourceId: source.sourceId },
+                    }}
+                    className={clsx(
+                      "group/source flex grow items-center gap-x-2 hover:bg-vivid-primary @[1024px]/layout:hover:bg-obsidian-lighter",
+                      "p-4 @[1024px]/layout:py-2"
+                    )}
+                  >
+                    <YoutubePictogram
+                      className={clsx(
+                        "h-4 w-4 text-snow-darker group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    />
+                    <div
+                      className={clsx(
+                        "font-mono text-xs text-snow-primary group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    >
+                      {source.sourceId}
+                    </div>
+                  </Link>
+                </div>
+              ))}
+              {bilibiliSources.map((source) => (
+                <div
+                  key={source.id}
+                  className={clsx(
+                    "flex items-center gap-x-2 border-l-2 !border-l-bilibili-primary"
+                  )}
+                >
+                  <Link
+                    href={{
+                      pathname: `/videos/${serial}`,
+                      query: {
+                        platform: "bilibili",
+                        sourceId: source.sourceId,
+                      },
+                    }}
+                    className={clsx(
+                      "group/source flex grow items-center gap-x-2 hover:bg-vivid-primary @[1024px]/layout:hover:bg-obsidian-lighter",
+                      "p-4 @[1024px]/layout:py-2"
+                    )}
+                  >
+                    <BilibiliPictogram
+                      className={clsx(
+                        "h-4 w-4 text-snow-darker group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    />
+                    <div
+                      className={clsx(
+                        "font-mono text-xs text-snow-primary group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    >
+                      {source.sourceId}
+                    </div>
+                  </Link>
+                </div>
+              ))}
+              {soundcloudSources.map((source) => (
+                <div
+                  key={source.id}
+                  className={clsx(
+                    "flex items-center gap-x-2 border-l-2 !border-l-soundcloud-primary"
+                  )}
+                >
+                  <Link
+                    href={{
+                      pathname: `/videos/${serial}`,
+                      query: {
+                        platform: "soundcloud",
+                        sourceId: source.sourceId,
+                      },
+                    }}
+                    className={clsx(
+                      "group/source flex grow items-center gap-x-2 hover:bg-vivid-primary @[1024px]/layout:hover:bg-obsidian-lighter",
+                      "p-4 @[1024px]/layout:py-2"
+                    )}
+                  >
+                    <SoundcloudPictogram
+                      className={clsx(
+                        "h-4 w-4 text-snow-darker group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    />
+                    <div
+                      className={clsx(
+                        "font-mono text-xs text-snow-primary group-hover/source:text-obsidian-primary @[1024px]/layout:group-hover/source:text-snow-primary"
+                      )}
+                    >
+                      {source.sourceId}
+                    </div>
+                  </Link>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={clsx("grid grow grid-cols-3 flex-wrap gap-4")}>
-          <header className={clsx("col-span-full px-4")}>
-            <h1 className={clsx("text-2xl font-bold text-snow-primary")}>
-              <MadPageLink fragment={video}>{video.title}</MadPageLink>
-            </h1>
-          </header>
+        </nav>
+        <div
+          className={clsx(
+            "relative z-0 grid grow gap-2",
+            "grid-cols-1 @[1024px]/layout:grid-cols-3"
+          )}
+        >
+          <section
+            className={clsx(
+              "col-span-full flex flex-col gap-y-2 rounded-md border border-obsidian-primary bg-obsidian-darker p-4"
+            )}
+          >
+            <VideoThumbnail
+              fragment={video}
+              imageSize="large"
+              className={clsx("mx-auto h-full max-h-[540px] max-w-[960px]")}
+            />
+          </section>
           <section
             className={clsx(
               "col-span-2 flex flex-col gap-y-2 rounded-md border border-obsidian-primary bg-obsidian-darker p-4"
